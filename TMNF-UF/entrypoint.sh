0379a966c864259b5b7bec2d2a64512edcfc1ecf
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-05-16 21:31:17 +0200

    Delete entrypoint.sh

--- TMNF-UF/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Startup Variables
-MODIFIED_STARTUP=`/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/maplist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit`
-echo ":/home/container/tmnf-uf$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-${MODIFIED_STARTUP}
