0a4e0da854a9e0ea656e1a3601a29eee67bfb32c
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-05-16 20:13:45 +0200

    Update start-server.sh

--- TMNF-UF/Config/start-server.sh
+++ TMNF-UF/Config/start-server.sh
@@ -2,3 +2,4 @@
 cd /home/container/tmnf-uf/
 /home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit
 sleep 5s
+# Hier muss noch xseco folgen
