e5d1d4adf9e1650b63f48afb79de18d909922b96
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-05-17 14:22:12 +0200

    Add files via upload

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/ash
+cd /home/container
 
-# Startup Variables
-MODIFIED_STARTUP=`/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/maplist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit`
-echo ":/home/container/tmnf-uf$ ${MODIFIED_STARTUP}"
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
