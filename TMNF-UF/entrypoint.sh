191d16c39c7874d7ec0c8695e10ce36ea96bbed8
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-05-17 17:21:05 +0200

    Add files via upload

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -1,12 +1,8 @@
-#!/bin/ash
-cd /home/container
-
-# Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-eval ${MODIFIED_STARTUP}
+#!/bin/bash
+# Start TM-Server
+cd /home/container/tmnf-uf/
+/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit
+sleep 5s
+# Start XAseco
+cd /home/container/xaseco/
+/home/container/xaseco/Aseco.sh
