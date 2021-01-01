72240a1d0586ba89d7a115d5e88fcbff71d34bb6
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2021-01-01 17:27:26 +0100

    Update entrypoint.sh

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -1,8 +1,19 @@
+# Test-Stuff
 #!/bin/bash
 # Start TM-Server
-cd /home/container/tmnf-uf/
-/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit
-sleep 5s
+#cd /home/container/tmnf-uf/
+#/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit
+#sleep 5s
 # Start XAseco
-cd /home/container/xaseco/
-/home/container/xaseco/Aseco.sh
+#cd /home/container/xaseco/
+#/home/container/xaseco/Aseco.sh
+
+#!/bin/bash
+cd /home/container
+
+#STARTUP = MODIFIED_STARTUP
+MODIFIED_STARTUP=eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container/$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
