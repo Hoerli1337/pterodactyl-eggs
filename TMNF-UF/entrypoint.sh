df3fe3168e09ff6ee2a6eecb7d61321c5db19554
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2021-01-01 23:27:37 +0100

    Update entrypoint.sh

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -14,7 +14,7 @@ cd /mnt/server
 
 #STARTUP = MODIFIED_STARTUP
 MODIFIED_STARTUP=eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container/$ ${MODIFIED_STARTUP}"
+echo ":/mnt/server/$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
