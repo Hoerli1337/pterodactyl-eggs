0a0824b310dc5c033e37b4790868a5034901c37f
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2021-01-02 13:10:27 +0100

    Update entrypoint.sh

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -1,15 +1,4 @@
-# Test-Stuff
 #!/bin/bash
-# Start TM-Server
-#cd /home/container/tmnf-uf/
-#/home/container/tmnf-uf/TrackmaniaServer /game_settings=MatchSettings/tracklist.txt /dedicated_cfg=dedicated_cfg.txt /noautoquit
-#sleep 5s
-# Start XAseco
-#cd /home/container/xaseco/
-#/home/container/xaseco/Aseco.sh
-
-#!/bin/bash
-# cd /home/container
 cd /home/container
 
 # Make internal Docker IP address available to processes.
