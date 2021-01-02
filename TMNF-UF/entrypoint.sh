daa9748f561dd2eb3e8a2ebfc4ee7e76aeb42081
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2021-01-02 11:36:28 +0100

    Update entrypoint.sh

--- TMNF-UF/entrypoint.sh
+++ TMNF-UF/entrypoint.sh
@@ -10,11 +10,14 @@
 
 #!/bin/bash
 # cd /home/container
-cd /mnt/server
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 #STARTUP = MODIFIED_STARTUP
-MODIFIED_STARTUP=eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/mnt/server/$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
